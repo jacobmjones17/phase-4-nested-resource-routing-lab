5c194360ef1228e6e987761237f98f25cfdc4586
Jacob <jacobmjones17@gmail.com>
2022-10-08 17:01:21 -0400

    commit tree c7395321994adac04a29decc34b7731686b3933c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+  
+  def items_index
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items, include: :user
+  end
 
 end
